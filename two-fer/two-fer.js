dc09928c5dcc8218093a83aaad4ecb05f1ca408f
Reis <32417584+felipesdreis@users.noreply.github.com>
2020-11-03 11:13:44 -0300

    New exercises

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,11 +1,6 @@
-function twoFer(name) {
-
-  if (name) {
-    console.log(`One for ${name}, one for me.`);
-  } else {
-    console.log('One for you, one for me.');
-  }
-
+export function twoFer(name = "you") {
+  return `One for ${name}, one for me.`;
 }
+
 
 
